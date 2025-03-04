b133de70639776f68b49a152c4e81a101009cd80
nailton-vieira <nailtonvieira@gmail.com>
2025-03-04 19:47:02 -0300

    Enviando base ao repo

---
+++ src/main/java/br/com/nailtonvieira/spring_security_jwt/SpringsecurjwtApplication.java
@@ -0,0 +1,13 @@
+package br.com.nailtonvieira.spring_security_jwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringsecurjwtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringsecurjwtApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/nailtonvieira/spring_security_jwt/SpringsecurjwtApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.nailtonvieira.spring_security_jwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringsecurjwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
