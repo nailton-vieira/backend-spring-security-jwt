ee0b79cb996febb5b8e1e080e073d071d436706e
nailton-vieira <nailtonvieira@gmail.com>
2025-03-04 20:47:15 -0300

    Enviando o componente Filtro de segurança ao repo

--- src/main/java/br/com/nailtonvieira/spring_security_jwt/repository/UserRepository.java
+++ src/main/java/br/com/nailtonvieira/spring_security_jwt/repository/UserRepository.java
@@ -1,9 +1,13 @@
 package br.com.nailtonvieira.spring_security_jwt.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import br.com.nailtonvieira.spring_security_jwt.domain.user.User;
 
 public interface UserRepository extends JpaRepository<User, String> {
+    
+    Optional<User> findByEmail(String email);
 
 }
